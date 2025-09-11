161052b5c3f1e623e14375cb26cf00083a848cf7
Divyarora0906 <divyarora0906@gmail.com>
2025-09-12 01:19:47 +0530

    Added Left Node

---
+++ Tree/LeftNode.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+struct Node *CreateNode(int data)
+{
+    struct Node *p = (struct Node *)malloc(sizeof(struct Node));
+    p->data = data;
+    p->left = NULL;
+    p->right = NULL;
+    return p;
+}
+void PrintLeaf(struct Node *Root)
+{
+    if (Root == NULL) return;
+
+    printf("%d \n", Root->data);
+
+    if (Root->left != NULL)
+    {
+        PrintLeaf(Root->left);
+    }
+}
+int main()
+{
+    struct Node *Root = CreateNode(15);
+    struct Node *L1 = CreateNode(10);
+    struct Node *R1 = CreateNode(5);
+    struct Node *Left = CreateNode(11);
+    struct Node *LeftRight = CreateNode(7);
+    struct Node *Right = CreateNode(99);
+    struct Node *RightRight = CreateNode(100);
+    Root->left = L1;
+    Root->right = R1;
+    L1->left = Left;
+    L1->right = LeftRight;
+    R1->left = Right;
+    R1->right = RightRight;
+    PrintLeaf(Root);
+    return 0;
+}
